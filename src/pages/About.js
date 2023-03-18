0e7fa576ede830c73b70ca1902098520dd2f9b40
AhmedGamalG2 <ememe22@gmail.com>
2023-03-18 17:03:14 +0200

    new commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,18 @@ const About = () => {
       <img className={mod.img} src="https://i.ibb.co/BG2Yf5Z/IMG-7537.jpg" />
       <h1 className={mod.name}>Ahmed Gamal</h1>
       <p className={mod.para}>
-        My name is Ahmed Gamal i'm a Front End Developer living in Cairo
+        <div>
+          My name is Ahmed Gamal i'm a Front End Developer living in Cairo
+        </div>
+        <div className={mod.links}>
+          <a href="https://github.com/AhmedGamalG2">
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ahmed-gamal-a8009b265">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a>Portfolio</a>
+        </div>
       </p>
     </div>
   );
